d237b9c1c7fe6d931770e1f2e454d9c71c64f2bd
Rodgers Angala <angalarodgers@gmail.com>
2023-03-04 10:04:50 +0300

    a

--- customer/src/context/authContext.jsx
+++ customer/src/context/authContext.jsx
@@ -11,7 +11,7 @@ export const AuthContextProvider = ({ children }) => {
   const login = async (inputs) => {
     try {
       const res = await axios.post(
-        "http://localhost:8880/api/auth/login",
+        "http://api.prowritingservice.net/api/auth/login",
         inputs,
         {
           withCredentials: true,
