1cd25f29b2d01a6ab1b4f05964b0878b585eaa0b
Rodgers Angala <angalarodgers@gmail.com>
2023-03-05 10:16:58 +0300

    Update inputs

--- customer/src/pages/create-order/CraeteOrderForm.jsx
+++ customer/src/pages/create-order/CraeteOrderForm.jsx
@@ -61,6 +61,8 @@ const CraeteOrderForm = () => {
     e.preventDefault();
     if (inputs.topic.length === 0) {
       toast.error("Topic reqyured!");
+    } else if (inputs.discipline.length === 0) {
+      toast.error("Discipline required!");
     } else {
       toast.success("Submitting...");
       try {
@@ -129,11 +131,95 @@ const CraeteOrderForm = () => {
                               name="paper_type"
                               onChange={handleChange}
                             >
-                              <option>1</option>
-                              <option>2</option>
-                              <option>3</option>
-                              <option>4</option>
-                              <option>5</option>
+                              <option value="" disabled>
+                                Type Of Paper
+                              </option>
+                              <option value="Admission Essay">
+                                Admission Essay
+                              </option>
+                              <option value="Annotated Bibliography">
+                                Annotated Bibliography
+                              </option>
+                              <option value="Argumentative essay">
+                                Argumentative essay
+                              </option>
+                              <option value="Article Review">
+                                Article Review
+                              </option>
+                              <option value="Article Writing">
+                                Article Writing
+                              </option>
+                              <option value="Assignment (Any type)">
+                                Assignment (Any type)
+                              </option>
+                              <option value="Book/Movie Review">
+                                Book/Movie Review
+                              </option>
+                              <option value="Business Plan">
+                                Business Plan
+                              </option>
+                              <option value="Business Proposal">
+                                Business Proposal
+                              </option>
+                              <option value="Capstone Project">
+                                Capstone Project
+                              </option>
+                              <option value="Case Study">Case Study</option>
+                              <option value="Coursework">Coursework</option>
+                              <option value="Creative Writing">
+                                Creative Writing
+                              </option>
+                              <option value="Critical Thinking / Review">
+                                Critical Thinking / Review
+                              </option>
+                              <option value="Discussion Post">
+                                Discussion Post
+                              </option>
+                              <option value="Essay (any type)">
+                                Essay (any type)
+                              </option>
+                              <option value="Homework">Homework</option>
+                              <option value="Lab Report">Lab Report</option>
+                              <option value="Letter/Memo">Letter/Memo</option>
+                              <option value="Literature Review">
+                                Literature Review
+                              </option>
+                              <option value="Marketing Plan">
+                                Marketing Plan
+                              </option>
+                              <option value="Multi-choice questions">
+                                Multi-choice questions
+                              </option>
+                              <option value="Other">Other</option>
+                              <option value="Outline">Outline</option>
+                              <option value="PowerPoint Presentation (with speaker notes)">
+                                PowerPoint Presentation (with speaker notes)
+                              </option>
+                              <option value="Reaction Paper">
+                                Reaction Paper
+                              </option>
+                              <option value="Reflective Writing">
+                                Reflective Writing
+                              </option>
+                              <option value="Research Paper">
+                                Research Paper
+                              </option>
+                              <option value="Research Proposal">
+                                Research Proposal
+                              </option>
+                              <option value="Script writing">
+                                Script writing
+                              </option>
+                              <option value="Short Answer Questions">
+                                Short Answer Questions
+                              </option>
+                              <option value="SWOT Analysis">
+                                SWOT Analysis
+                              </option>
+                              <option value="Term Paper">Term Paper</option>
+                              <option value="Thesis/Dissertation">
+                                Thesis/Dissertation
+                              </option>
                             </select>
                           </div>
 
@@ -147,11 +233,154 @@ const CraeteOrderForm = () => {
                               name="discipline"
                               onChange={handleChange}
                             >
-                              <option>1</option>
-                              <option>2</option>
-                              <option>3</option>
-                              <option>4</option>
-                              <option>5</option>
+                              <option
+                                value={""}
+                                style={{ color: "darkblue" }}
+                                disabled
+                              >
+                                Non-technicals Disciplines
+                              </option>
+                              <option value="Advertising & Marketing">
+                                Advertising & Marketing
+                              </option>
+                              <option value="Agriculture">Agriculture</option>
+                              <option value="Anatomy">Anatomy</option>
+                              <option value="Anthropology">Anthropology</option>
+                              <option value="Architecture">Architecture</option>
+                              <option value="Art">Art</option>
+                              <option value="Astronomy">Astronomy</option>
+                              <option value="Aviation">Aviation</option>
+                              <option value="Biology">Biology</option>
+                              <option value="Business & Management">
+                                Business & Management
+                              </option>
+                              <option value="Communications & Media">
+                                Communications & Media
+                              </option>
+                              <option value="Computer Science">
+                                Computer Science
+                              </option>
+                              <option value="Creative Writing">
+                                Creative Writing
+                              </option>
+                              <option value="Criminology">Criminology</option>
+                              <option value="Cultural & Tourism">
+                                Cultural & Tourism
+                              </option>
+                              <option value="Drama & Theatre">
+                                Drama & Theatre
+                              </option>
+                              <option value="Economics">Economics</option>
+                              <option value="Education">Education</option>
+                              <option value="Engineering">Engineering</option>
+                              <option value="English & Literature">
+                                English & Literature
+                              </option>
+                              <option value="Entrepreneurship">
+                                Entrepreneurship
+                              </option>
+                              <option value="Environmental Science">
+                                Environmental Science
+                              </option>
+                              <option value="Ethics">Ethics</option>
+                              <option value="Fashion">Fashion</option>
+                              <option value="Finance & Accounting">
+                                Finance & Accounting
+                              </option>
+                              <option value="Food & Nutrition">
+                                Food & Nutrition
+                              </option>
+                              <option value="Forensic Science">
+                                Forensic Science
+                              </option>
+                              <option value="Geography">Geography</option>
+                              <option value="Geology">Geology</option>
+                              <option value="Health & Medicine">
+                                Health & Medicine
+                              </option>
+                              <option value="Healthcare & Life Sciences">
+                                Healthcare & Life Sciences
+                              </option>
+                              <option value="History">History</option>
+                              <option value="Human Relations">
+                                Human Relations
+                              </option>
+                              <option value="Information Technology (IT)">
+                                Information Technology (IT)
+                              </option>
+                              <option value="International Affairs/Relations">
+                                International Affairs/Relations
+                              </option>
+                              <option value="Journalism & Telecommunication">
+                                Journalism & Telecommunication
+                              </option>
+                              <option value="Law">Law</option>
+                              <option value="Linguistics">Linguistics</option>
+                              <option value="Logistics">Logistics</option>
+                              <option value="Military Science">
+                                Military Science
+                              </option>
+                              <option value="Music">Music</option>
+                              <option value="Natural Science">
+                                Natural Science
+                              </option>
+                              <option value="Nursing">Nursing</option>
+                              <option value="Others">Others</option>
+                              <option value="Pharmacology">Pharmacology</option>
+                              <option value="Philosophy">Philosophy</option>
+                              <option value="Photography">Photography</option>
+                              <option value="Physiology">Physiology</option>
+                              <option value="Political Science">
+                                Political Science
+                              </option>
+                              <option value="Psychology">Psychology</option>
+                              <option value="Public Administration">
+                                Public Administration
+                              </option>
+                              <option value="Public Relations">
+                                Public Relations
+                              </option>
+                              <option value="Religion & Theology">
+                                Religion & Theology
+                              </option>
+                              <option value="Sociology">Sociology</option>
+                              <option value="Sports & Athletics">
+                                Sports & Athletics
+                              </option>
+                              <option value="Urban & Environmental Planning">
+                                Urban & Environmental Planning
+                              </option>
+                              <option value="Veterinary Science">
+                                Veterinary Science
+                              </option>
+                              <option
+                                value=""
+                                style={{ color: "darkblue" }}
+                                disabled
+                              >
+                                Technical Discipline
+                              </option>
+                              <option value="Accounting & Finance">
+                                Accounting & Finance
+                              </option>
+                              <option value="Calculus">Calculus</option>
+                              <option value="Chemistry">Chemistry</option>
+                              <option value="Coding & Programming">
+                                Coding & Programming
+                              </option>
+                              <option value="C#">C#</option>
+                              <option value="C++">C++</option>
+                              <option value="Java">Java</option>
+                              <option value="PHP">PHP</option>
+                              <option value="Python">Python</option>
+                              <option value="SQL">SQL</option>
+                              <option value="Excel">Excel</option>
+                              <option value="Mathematics, Geometry & Algebra">
+                                Mathematics, Geometry & Algebra
+                              </option>
+                              <option value="Physics">Physics</option>
+                              <option value="Statistics">Statistics</option>
+                              <option value="Web design">Web design</option>
                             </select>
                           </div>
 
@@ -165,11 +394,14 @@ const CraeteOrderForm = () => {
                               name="academic_level"
                               onChange={handleChange}
                             >
-                              <option>1</option>
-                              <option>2</option>
-                              <option>3</option>
-                              <option>4</option>
-                              <option>5</option>
+                              <option value="">Select...</option>
+                              <option value={"High School"}>High School</option>
+                              <option value={"College"}>College</option>
+                              <option value={"Undergraduate"}>
+                                Undergraduate
+                              </option>
+                              <option value={"Master"}>Master</option>
+                              <option value={"PhD"}>PhD</option>
                             </select>
                           </div>
 
@@ -304,11 +536,10 @@ const CraeteOrderForm = () => {
                           name="service"
                           onChange={handleChange}
                         >
-                          <option>1</option>
-                          <option>2</option>
-                          <option>3</option>
-                          <option>4</option>
-                          <option>5</option>
+                          <option>Select Type Of Service</option>
+                          <option value={"writing"}>Writing</option>
+                          <option value={"re-writing"}>Re-Writing</option>
+                          <option value={"editing"}>Editing</option>
                         </select>
                       </div>
 
@@ -322,11 +553,19 @@ const CraeteOrderForm = () => {
                           name="style"
                           onChange={handleChange}
                         >
-                          <option>1</option>
-                          <option>2</option>
-                          <option>3</option>
-                          <option>4</option>
-                          <option>5</option>
+                          <option value="" disabled>
+                            Select Style
+                          </option>
+                          <option value="AAA">AAA</option>
+                          <option value="APA">APA</option>
+                          <option value="APSA">APSA</option>
+                          <option value="ASA">ASA</option>
+                          <option value="Chicago (Turabian) author-date">
+                            Chicago (Turabian) author-date
+                          </option>
+                          <option value="CSE name-year">CSE name-year</option>
+                          <option value="Harvard">Harvard</option>
+                          <option value="MLA">MLA</option>
                         </select>
                       </div>
                     </form>
