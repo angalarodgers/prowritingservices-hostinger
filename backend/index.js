67129c7bb5a02aba360253f910d93b28ea2d95b0
Rodgers Angala <angalarodgers@gmail.com>
2023-03-05 07:33:01 +0300

    allow

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,10 @@ import messageRoutes from "./routes/messages.js";
 //middleware
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 app.use(express.json());
--- customer/src/axios.jsx
+++ customer/src/axios.jsx
@@ -1,6 +1,11 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: "https://api.prowritingservice.net/api/",
+  baseURL: "http://89.116.212.238:8800/api/",
+
   withCredentials: true,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+  },
 });
